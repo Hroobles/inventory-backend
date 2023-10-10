e43376b24a85ac1e675ff009c1b1c8cf6cb8c5f4
Hernán Robles <h_roobles@MacBook-Air-de-Hernan.local>
2023-10-10 23:09:51 +0200

    servicio guardar categoria

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -75,8 +75,38 @@ public class CategoryServiceImpl implements ICategoryService {
 		
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
-	
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		try {
+			Category categorySaved = categoryDao.save(category);
+				
+			if (categorySaved != null) {
+				list.add(categorySaved);
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Response ok", "00","Categoria guardada");
+				
+			} else {
+				response.setMetadata("Response nok", "-1","Categoria no guardada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}
+			
+		} catch (Exception e) {
+			
+			response.setMetadata("Response nok", "-1","Error al grabar categoria");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	
 	}
+	
+	
+}
 
 
