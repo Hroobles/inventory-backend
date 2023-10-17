8e0d708bc2ca8d4fa46f7f827459b1e4088b01e2
Hernán Robles <h_roobles@MacBook-Air-de-Hernan.local>
2023-10-17 23:27:22 +0200

    servicio eliminar categoria

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -149,6 +149,29 @@ public class CategoryServiceImpl implements ICategoryService {
 	
 	
 	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+		CategoryResponseRest response = new CategoryResponseRest();
+		
+		try {
+			
+			categoryDao.deleteById(id);
+			response.setMetadata("Respuesta ok", "00", "Registro eliminado");
+			
+		} catch (Exception e) {
+			
+			response.setMetadata("Response nok", "-1","Error al eliminar categoria");
+			e.getStackTrace();
+			
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	
+	
+	}
 	
 	
 }
